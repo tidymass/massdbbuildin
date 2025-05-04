ac2dce6302ff9c9bb08238655693d0872d46c2c4
ShawnWx2009 <shawnwang2016@126.com>
2025-05-05 01:32:14 +0800

    init commit

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' @title MoNA - MassBank of North America database
+#' @description MassBank of North America database
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name mona_ms2
+#' @format A databaseClass object.
+
+"mona_ms2"
+
+##------------------------------------------------------------------------------
+#' @title massbank database database
+#' @description massbank database database
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name massbank_ms2
+#' @format A databaseClass object.
+
+"massbank_ms2"
+
+##------------------------------------------------------------------------------
+#' @title Human Metabolome Database
+#' @description HMDB
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name hmdb_ms2
+#' @format A databaseClass object.
+
+"hmdb_ms2"
+
+
---
+++ README.md
@@ -0,0 +1,16 @@
+## Description
+
+Commonly used MS2 metabolite databases include HMDB, MoNA, and MassBank. For other types of databases, please download them from:
+
+https://www.tidymass.org/databases/
+
+## install
+
+```r
+if(!require(remotes)){
+install.packages("remotes")
+}
+remotes::install_github("tidymass/massdbbuildin")
+
+```
+
